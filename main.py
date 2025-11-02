1b85e29b04826848cb9a11f69773bea7d94232a8
saofns <119787120+saofns@users.noreply.github.com>
2025-11-02 15:31:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger # 使用 astrbot 提供的 logger 接口
+from astrbot.api import logger
 import astrbot.api.message_components as Comp
 
 @register("pic_sen", "author", "图片发送", "1.0.0", "https://github.com/saofns/astrbot_plugin_test")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-
     
-    @filter.command(“pic”)
+    @filter.command("pic")  # 使用英文引号
     async def helloworld(self, event: AstrMessageEvent):
-       chain = [
-        Comp.At(qq=event.get_sender_id()), # At 消息发送者
+        chain = [
+            Comp.At(qq=event.get_sender_id()),  # At 消息发送者
+            Comp.Plain(" "),
+            # 注意：修正图片加载方法
+            Comp.Image.fromURL("https://imgchr.com/i/pVzbqV1"),  # 使用英文引号
+            Comp.Plain("这是一个图片。")  # 使用英文引号
+        ]
         
-       
-        Comp.Image.fromFileSystem("https://imgchr.com/i/pVzbqV1"), # 从本地文件目录发送图片
-        Comp.Plain("这是一个图片。")
-    ]
-
-   yield event.chain_result(chain)
+        return event.chain_result(chain)
