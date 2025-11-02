80df3173934fb40864acdd06ab417b5c937767c2
saofns <119787120+saofns@users.noreply.github.com>
2025-11-02 16:00:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("dongzhuo_cmd", "author", "董卓指令插件", "1.0.0", "https://github.com/saofns/astrbot_plugin_test")
+@register("dongzhuo_reply", "author", "董卓回复插件", "1.0.0", "https://github.com/saofns/astrbot_plugin_test")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
+        logger.info("董卓回复插件已加载")
     
-    @filter.command("董卓")
-    async def dongzhuo_command(self, event: AstrMessageEvent):
-        """董卓指令 - 使用 /董卓 触发"""
+    @filter.keyword("董卓")
+    async def dongzhuo_reply(self, event: AstrMessageEvent):
+        """当消息包含'董卓'时回复'何意味'"""
         return event.plain_result("何意味")
-
