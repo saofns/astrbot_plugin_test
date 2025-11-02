6563edd7e6de46ba4e441f9a437bca0eb38c8179
saofns <119787120+saofns@users.noreply.github.com>
2025-11-02 15:06:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ class MyPlugin(Star):
         super().__init__(context)
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    @filter.command("董卓")
     async def helloworld(self, event: AstrMessageEvent):
         '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
         user_name = event.get_sender_name()
         message_str = event.message_str # 获取消息的纯文本内容
-        logger.info("董卓")
-        yield event.plain_result(f"和意味") # 发送一条纯文本消息
+        logger.info("触发hello world指令!")
+        yield event.plain_result(f"fuck you") # 发送一条纯文本消息
 
     async def terminate(self):
         '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
