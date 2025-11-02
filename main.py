8f01831150c0f945c135547bd16025f626b35416
saofns <119787120+saofns@users.noreply.github.com>
2025-11-02 15:10:36 +0800

    Update command aliases for helloworld function

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MyPlugin(Star):
         super().__init__(context)
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("董卓")
+    @filter.command("dz", alias={'董卓', '吕布'})
     async def helloworld(self, event: AstrMessageEvent):
         '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
         user_name = event.get_sender_name()
